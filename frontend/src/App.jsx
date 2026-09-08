5bc1d4010df66150d32820f8e688dc3e7c3019c9
Akash Sharma <skysharma777@gmail.com>
2026-09-08 21:56:48 +0530

    feat: add frontend login

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,6 +2,7 @@ import { Routes, Route } from "react-router-dom"
 import Land from "./pages/Land.jsx"
 import CreateNote from "./pages/CreateNote.jsx"
 import Notes from "./pages/Notes.jsx"
+import Login from "./pages/Login.jsx"
 
 
 function App() {
@@ -13,8 +14,10 @@ function App() {
     
     <Routes>
       <Route path = '/' element={<Land/>} />
+      <Route path = '/login' element={<Login/>} />
       <Route path = '/create' element={<CreateNote/>} />
-      <Route path = '/notes' element={<Notes/>} />  
+      <Route path = '/notes' element={<Notes/>} />
+      
     </Routes>
   )
 }
---
+++ frontend/src/pages/Login.jsx
@@ -0,0 +1,84 @@
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import axios from "axios";
+
+const Login = () => {
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [error, setError] = useState("");
+    const [isLoading, setIsLoading] = useState(false);
+
+    const navigate = useNavigate();
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+
+        setError("");
+        setIsLoading(true);
+
+        try {
+            const response = await axios.post(
+                "http://localhost:3000/api/auth/login",{
+                    email,
+                    password,
+                }
+            )
+
+            const data = response.data
+
+            localStorage.setItem("token", data.token)
+            localStorage.setItem("user", JSON.stringify(data.user))
+
+            navigate("/notes")
+
+        } catch (error) {
+            setError(
+                error.response?.data?.message || "Login failed."
+            )
+        } finally{
+            setIsLoading(false)
+        }
+    };
+
+    return (
+        <div className="min-h-screen text-xl flex flex-col justify-center items-center gap-12">
+            <h1 className="text-[#8881DD] font-extrabold tracking-wider text-4xl">Log In</h1>
+
+            <form onSubmit={handleSubmit} className="flex flex-col justify-center gap-12">
+                <div>
+                    <label htmlFor="email" className="px-4 font-semibold">Email</label>
+
+                    <input
+                        id="email"
+                        type="email"
+                        value={email}
+                        onChange={(e) => setEmail(e.target.value)}
+                        required
+                        className="bg-slate-400 px-4 py-2 rounded-md outline-none"
+                    />
+                </div>
+
+                <div>
+                    <label htmlFor="password" className="px-4 font-semibold">Password</label>
+
+                    <input
+                        id="password"
+                        type="text"
+                        value={password}
+                        onChange={(e) => setPassword(e.target.value)}
+                        required
+                        className="bg-slate-400 px-4 py-2 rounded-md outline-none"
+                    />
+                </div>
+
+                {error && <p>{error}</p>}
+
+                <button type="submit" disabled={isLoading} className="bg-[#8881DD] px-4 py-3 rounded-md text-white font-bold text-2xl border w-full">
+                    {isLoading ? "Logging in..." : "Log In"}
+                </button>
+            </form>
+        </div>
+    );
+};
+
+export default Login;
