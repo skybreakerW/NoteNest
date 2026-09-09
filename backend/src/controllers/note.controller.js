c057550d48d4496f41f831b8c39e169e781826a6
Akash Sharma <skysharma777@gmail.com>
2026-09-09 07:16:25 +0530

    feat: add note update api

--- backend/src/controllers/note.controller.js
+++ backend/src/controllers/note.controller.js
@@ -48,4 +48,34 @@ const deleteNote = async(req, res) => {
     })
 }
 
-export { createNote, getNotes, deleteNote }
+
+const updateNote = async(req,res) => {
+    const { id } = req.params
+    const { title, description } = req.body
+
+    const note = await Note.findOneAndUpdate({
+        _id: id,
+        user: req.user._id,
+    },
+    {
+        title,
+        description,    
+    },
+    {
+        new: true,
+        runValidators: true
+    })
+
+    if(!note){
+        return res.status(404).json({
+            message: "Note not found.",
+        })
+    }
+    
+    res.status(200).json({
+        message: "Note updated successfully."
+    })
+
+}
+
+export { createNote, getNotes, deleteNote, updateNote }
--- backend/src/routes/note.route.js
+++ backend/src/routes/note.route.js
@@ -1,6 +1,6 @@
 import express from "express"
 import { authMiddleware } from "../middlewares/auth.middleware.js"
-import { createNote, getNotes, deleteNote } from "../controllers/note.controller.js"
+import { createNote, getNotes, deleteNote, updateNote } from "../controllers/note.controller.js"
 
 const router = express.Router()
 
@@ -9,5 +9,6 @@ router.use(authMiddleware)
 router.post("/", createNote)
 router.get("/", getNotes)
 router.delete("/:id", deleteNote)
+router.patch("/:id", updateNote)
 
 export { router as noteRouter }
