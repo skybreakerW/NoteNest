668d5fc0f65a0097cb9be54454f774006aa7ae82
Akash Sharma <skysharma777@gmail.com>
2026-09-11 00:27:09 +0530

    refactor: route changed to /

--- frontend/src/components/ProtectedRoute.jsx
+++ frontend/src/components/ProtectedRoute.jsx
@@ -7,7 +7,7 @@ const ProtectedRoute = ({children}) => {
     const { token } = useContext(AuthContext)
 
     if(!token){
-        return <Navigate to="/login" replace/>
+        return <Navigate to="/" replace/>
     }
 
   return children 
